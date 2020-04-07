35e0a79ce7372b52a61fb02eff3ab7f6cd5e4096
Heather Moreira <moreiraheather@gmail.com>
2020-04-07 04:03:19 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -33,33 +33,9 @@ class CashRegister
   
   def void_last_transaction 
     self.items = self.items - self.last_transaction 
+    binding.pry 
   end 
  
-#   describe '#items' do
-#     it 'returns an array containing all items that have been added' do
-#       new_register = CashRegister.new
-#       new_register.add_item("eggs", 1.99)
-#       new_register.add_item("tomato", 1.76, 3)
-#       expect(new_register.items).to eq(["eggs", "tomato", "tomato", "tomato"])
-#     end
-#   end
-
-#   describe '#void_last_transaction' do
-#     it 'subtracts the last item from the total' do
-#       cash_register.add_item("apple", 0.99)
-#       cash_register.add_item("tomato", 1.76)
-#       cash_register.void_last_transaction
-#       expect(cash_register.total).to eq(0.99)
-#     end
-
-#     it 'returns the total to 0.0 if all items have been removed' do
-#       cash_register.add_item("tomato", 1.76, 2)
-#       expect{cash_register.void_last_transaction}.to change{cash_register.total}.from(3.52).to(0.0)
-#     end
-#   end
-# end
-
-
   
 end 
    
