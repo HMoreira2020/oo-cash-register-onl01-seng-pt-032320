84544faea2a715c065498a1b5f53e81e09394474
Heather Moreira <moreiraheather@gmail.com>
2020-04-07 03:56:02 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,7 +13,7 @@ class CashRegister
   
   def add_item(title, price, quantity = 1)
     self.total += price * quantity
-    @items << title * quantity 
+    @items << title * quantity.split("") 
   end 
   
   def apply_discount
